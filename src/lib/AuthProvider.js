5e3b0e58526aabd6989a39810d15c6cf30e788c4
arimagic-8bit <baggerman1994@gmail.com>
2020-07-16 11:33:46 +0200

    Create auth service and auth Provider. Need test.

---
+++ src/lib/AuthProvider.js
@@ -0,0 +1,124 @@
+import React from "react";
+import authService from "./authService";
+const { Consumer, Provider } = React.createContext();
+
+// --> HOC to create Consumer <-- //
+
+const withAuth = (WrappedComponent) => {
+  return class extends React.Component {
+    render() {
+      return (
+        <Consumer>
+          {({
+            isLoggedin,
+            user,
+            isLoading,
+            loginError,
+            signupError,
+            login,
+            logout,
+            signup,
+          }) => {
+            return (
+              <WrappedComponent
+                isLoggedin={isLoggedin}
+                user={user}
+                isLoading={isLoading}
+                loginError={loginError}
+                signupError={signupError}
+                login={login}
+                logout={logout}
+                signup={signup}
+                {...this.props}
+              />
+            );
+          }}
+        </Consumer>
+      );
+    }
+  };
+};
+
+// --> Provider <-- //
+
+class AuthProvider extends React.Component {
+  state = {
+    isLoggedin: false,
+    user: null,
+    isLoading: true, // the auth is still loading?
+    loginError: null,
+    signupError: null,
+  };
+
+  // when app and AuthProvider loads for first time
+  // call to '/me' and check if user is authenticated
+  componentDidMount() {
+    authService
+      .me()
+      .then((response) => {
+        const user = response.data;
+        this.setState({ isLoggedin: true, isLoading: false, user });
+      })
+      .catch(() =>
+        this.setState({ isLoggedin: false, user: null, isLoading: false })
+      );
+  }
+
+  signup = (username, password) => {
+    authService
+      .signup(username, password)
+      .then((response) => {
+        const user = response.data;
+        this.setState({ isLoggedin: true, isLoading: false, user });
+      })
+      .catch((err) => {
+        this.setState({
+          signupError:
+            "Sorry, there was a problem. Please, check the provided info and try again",
+        });
+      });
+  };
+
+  login = (username, password) => {
+    authService
+      .login(username, password)
+      .then((response) => {
+        const user = response.data;
+        this.setState({ isLoggedin: true, isLoading: false, user });
+      })
+      .catch((err) => {
+        this.setState({ loginError: "Incorrect username or password" });
+      });
+  };
+
+  logout = () => {
+    authService
+      .logout()
+      .then(() => this.setState({ isLoggedin: false, user: null }))
+      .catch((err) => console.log(err));
+  };
+
+  render() {
+    const { isLoggedin, user, isLoading, loginError, signupError } = this.state;
+    const { login, logout, signup } = this;
+
+    return (
+      <Provider
+        value={{
+          isLoggedin,
+          user,
+          isLoading,
+          loginError,
+          signupError,
+          login,
+          logout,
+          signup,
+        }}
+      >
+        {this.props.children}
+      </Provider>
+    );
+  }
+}
+
+export { withAuth, AuthProvider };
