a05a7a61a7e681f3647ab9b6cce6597d3810434c
arimagic-8bit <baggerman1994@gmail.com>
2020-07-16 19:00:58 +0200

    Add public and private routing on app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import {Switch, Route} from "react-router-dom";
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+import Splash from './pages/SplashScreen';
+import Signup from './pages/SignupScreen';
+import Login from './pages/LoginScreen';
+
+import Activities from './pages/ActivityScreen';
+import Error from './pages/ErrorScreen'
+
+import PublicRoute from './components/PublicRoute';
+import PrivateRoute from './components/PrivateRoute';
+
+class App extends Component {
+
+  render(){
+    return(
+      <div className='App'>
+        <Switch>
+          <PublicRoute exact path="/" component={Splash}/>
+          <PublicRoute exact path="/signup" component={Signup}/>
+          <PublicRoute exact path="/login" component={Login}/>
+
+          <PrivateRoute exact path="/activities" component={Activities}/>
+
+          <Route component={Error}/>
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App;
