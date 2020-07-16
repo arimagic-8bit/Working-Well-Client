90938f8e256ca3adc32fe0b0de2c255a25f96f70
arimagic-8bit <baggerman1994@gmail.com>
2020-07-16 12:14:57 +0200

    Set index with router and provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { AuthProvider } from "./lib/AuthProvider";
+
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </Router>,
   document.getElementById("root")
 );
